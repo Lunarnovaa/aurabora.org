cfe2bd082e4904870560ffda1bf858616f7fbec2
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-06-02 10:41:41 -0700

    replace time to read with word count

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
 import { defineConfig } from 'astro/config';
-import { remarkReadingTime } from './remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.aurabora.org',
-  markdown: {
-    remarkPlugins: [remarkReadingTime],
-  },
 });
